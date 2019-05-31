ae6656431205c76755bc6496228bd3010758fdce
BondarenkoP <BondarenkoP@competentum.ru>
2019-05-31 14:33:03 +0300

    router

--- config/aliases.js
+++ config/aliases.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 module.exports = {
   '@axios': path.resolve('src/config/axios'),
+  '@views': path.resolve('src/views'),
 };
--- src/common/App/App.js
+++ src/common/App/App.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import { root, branch } from 'baobab-react/higher-order';
-import { axs } from '@axios';
+import { Container } from 'react-bootstrap';
+import { Switch, Route, Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+import MainPage from '@views/mainPage';
 
 import store, { PARAMS } from '../store/index';
 
-import s from './index.module.css';
-import Button from 'react-bootstrap/Button';
+const history = createBrowserHistory();
 
 class App extends React.Component {
-  state = {
-    data: [],
-  };
-
-  componentDidMount() {
-    axs.get('/').then((resp) => {
-      this.setState({
-        data: resp.data,
-      });
-    });
-  }
-
   render() {
     return (
-      <div>
-        <Button>dsfgds</Button>
-        {this.state.data.map((el, key) => {
-          return (
-            <div className={s.app} key={el._id}>
-              {el.sname}
-            </div>
-          );
-        })}
-      </div>
+      <Container>
+        <Router history={history}>
+          <Switch>
+            <Route path='/' component={MainPage} />
+          </Switch>
+        </Router>
+      </Container>
     );
   }
 }
