f55044d5a537eef7a93a26a7bf8dcfe7b3795da8
BondarenkoP <BondarenkoP@competentum.ru>
2019-06-21 18:23:11 +0300

    security fix for token validation

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,5 +2,16 @@ import axios from 'axios';
 
 export const axs = axios.create({
   baseURL: 'http://localhost:5000/api',
-  headers: { 'x-access-token': localStorage.getItem('token') },
 });
+
+axs.interceptors.request.use(
+  (config) => {
+    if (localStorage.getItem('token') !== null) {
+      config.headers['x-access-token'] = localStorage.getItem('token');
+    }
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  },
+);
