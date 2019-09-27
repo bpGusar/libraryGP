3346ffdb7a0cba0058438d3a1b9187f1894ea54d
BondarenkoP <BondarenkoP@competentum.ru>
2019-09-27 17:02:06 +0300

    improvements

--- src/views/Layouts/Book.js
+++ src/views/Layouts/Book.js
@@ -7,26 +7,25 @@ import headerStyles from "@views/Header/Header.module.scss";
 
 export default function MainLayout(props) {
   const { children } = props;
-  console.log(headerStyles);
   return (
     <>
       <Responsive>
         <Segment
           inverted
           textAlign="center"
-          style={{ padding: "1em 0em" }}
+          style={{ padding: "1em 0em", zIndex: 1 }}
           vertical
         >
           <Header segmentClassName={headerStyles.segmentClass} />
-          <Image
-            fluid
-            style={{
-              position: "absolute",
-              top: 0
-            }}
-            src="http://localhost:5000/posters/fde8c4f4799fc918670df4bf906484e6.png"
-          />
         </Segment>
+        <Image
+          fluid
+          style={{
+            position: "absolute",
+            top: 0
+          }}
+          src="http://localhost:5000/posters/fde8c4f4799fc918670df4bf906484e6.png"
+        />
       </Responsive>
       <Container style={{ marginTop: "20px" }}>
         <div className="m-3">{children}</div>
